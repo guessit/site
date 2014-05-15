78f278e4920dbc94739323673946d6c0013b8922
Marlon Andrade <marlonmandrade@gmail.com>
2014-05-14 23:05:49 -0300

    adjusting games row

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,60 @@
 class HomeController < ApplicationController
   def index
+    @games = games.sample 4
+  end
+
+  private
+  def games
+    [
+      { 
+        image: 'technology.png',
+        title: 'Technology?',
+        sub: 'bla bla bla',
+        description: '',
+        url: ''
+      },
+      { 
+        image: 'country_flags.png',
+        title: 'Country Flags?',
+        sub: 'ble ble ble',
+        description: '',
+        url: ''
+      },
+      { 
+        image: 'brazilian_football.png',
+        title: 'Brazilian Footbal?',
+        sub: '',
+        description: '',
+        url: ''
+      },
+      { 
+        image: 'anime.png',
+        title: 'Anime?',
+        sub: '',
+        description: '',
+        url: ''
+      },
+      { 
+        image: 'dragon_ball.png',
+        title: 'Dragon Ball?',
+        sub: '',
+        description: '',
+        url: ''
+      },
+      { 
+        image: 'retro_games.png',
+        title: 'Retro Games?',
+        sub: '',
+        description: '',
+        url: ''
+      },
+      { 
+        image: 'vida_de_programador.png',
+        title: 'Vida de Programador?',
+        sub: '',
+        description: '',
+        url: ''
+      }
+    ]
   end
 end
