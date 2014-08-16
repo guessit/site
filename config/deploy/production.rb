535c4783fff645765fe84a746aa01862626fadc6
Marlon Andrade <marlonmandrade@gmail.com>
2014-08-16 17:01:30 -0300

    using server name instead of ip

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -15,7 +15,7 @@
 # server list. The second argument is a, or duck-types, Hash and is
 # used to set extended properties on the server.
 
-server '96.126.116.150', user: 'deploy', roles: %w{web app}
+server 'guessit.mobi', user: 'deploy', roles: %w{web app db}
 
 
 # Custom SSH Options
